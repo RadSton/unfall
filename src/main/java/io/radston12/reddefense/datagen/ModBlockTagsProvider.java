a635cf2f44d486d7e83a454496495abc63def4b1
radston12 <radston12@gmail.com>
2024-01-03 16:08:37 +0100

    Made CompressedBlocks immune to Wither and EnderDragon

--- src/main/java/io/radston12/reddefense/datagen/ModBlockTagsProvider.java
+++ src/main/java/io/radston12/reddefense/datagen/ModBlockTagsProvider.java
@@ -1,14 +1,18 @@
 package io.radston12.reddefense.datagen;
 
 import io.radston12.reddefense.RedDefenseMod;
+import io.radston12.reddefense.blocks.ModBlocks;
 import io.radston12.reddefense.blocks.compressed.CompressedBlocks;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.data.BlockTagsProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public class ModBlockTagsProvider extends BlockTagsProvider {
@@ -28,6 +32,16 @@ public class ModBlockTagsProvider extends BlockTagsProvider {
                 CompressedBlocks.COMPRESSED_IRON_BLOCK.get(),
                 CompressedBlocks.COMPRESSED_NETHERITE_BLOCK.get());
 
+        List<Block> b = new ArrayList<>();
+
         tag(BlockTags.SAND).add(CompressedBlocks.COMPRESSED_SAND.get());
+        for (String blockKey : ModBlocks.BLOCKS_MAP.keySet())
+            if (blockKey.startsWith("compressed"))
+                b.add(ModBlocks.BLOCKS_MAP.get(blockKey).get());
+
+        Block[] bArray = b.toArray(new Block[b.size()]);
+
+        tag(BlockTags.WITHER_IMMUNE).add(bArray);
+        tag(BlockTags.DRAGON_IMMUNE).add(bArray);
     }
 }
